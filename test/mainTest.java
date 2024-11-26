8fb76e173509b0d70e8d81d2704490b5165e53bd
eungyu04 <eunkyu7889@naver.com>
2024-11-27 07:02:57 +0900

    test 성공7 실패2

--- test/mainTest.java
+++ test/mainTest.java
@@ -13,13 +13,31 @@ class mainTest {
     }
     @Test
     void Test3() {
-        assertEquals(1, main.a);
+        assertEquals(0, main.a);
     }
     @Test
     void Test4() {
         assertEquals(1, main.a);
     }
-
-
+    @Test
+    void Test5() {
+        assertEquals(1, main.a);
+    }
+    @Test
+    void Test6() {
+        assertEquals(1, main.a);
+    }
+    @Test
+    void Test7() {
+        assertEquals(0, main.a);
+    }
+    @Test
+    void Test8() {
+        assertEquals(1, main.a);
+    }
+    @Test
+    void Test9() {
+        assertEquals(1, main.a);
+    }
 
 }
