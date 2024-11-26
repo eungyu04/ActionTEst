ea49652206280feea1cefb8aca217ca515ec2011
eungyu04 <eunkyu7889@naver.com>
2024-11-27 07:23:11 +0900

    그냥 원래대로, 테스트 아예 없을 때 컴파일에러 감지하는지

--- test/mainTest.java
+++ test/mainTest.java
@@ -3,41 +3,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class mainTest {
 
-    @Test
-    void Test1() {
-        assertEquals(1, main.a);
-    }
-    @Test
-    void Test2() {
-        assertEquals(1, main.a);
-    }
-    @Test
-    void Test3() {
-        assertEquals(0, main.a);
-    }
-    @Test
-    void Test4() {
-        assertEquals(1, main.a);
-    }
-    @Test
-    void Test5() {
-        assertEquals(1, main.a);
-    }
-    @Test
-    void Test6() {
-        assertEquals(1, main.a);
-    }
-    @Test
-    void Test7() {
-        assertEquals(0, main.a);
-    }
-    @Test
-    void Test8() {
-        assertEquals(1, main.a);
-    }
-    @Test
-    void Test9() {
-        assertEquals(1, main.a);
-    }
+//    @Test
+//    void Test1() {
+//        assertEquals(1, main.a);
+//    }
+//    @Test
+//    void Test2() {
+//        assertEquals(1, main.a);
+//    }
+//    @Test
+//    void Test3() {
+//        assertEquals(0, main.a);
+//    }
+//    @Test
+//    void Test4() {
+//        assertEquals(1, main.a);
+//    }
+//    @Test
+//    void Test5() {
+//        assertEquals(1, main.a);
+//    }
+//    @Test
+//    void Test6() {
+//        assertEquals(1, main.a);
+//    }
+//    @Test
+//    void Test7() {
+//        assertEquals(0, main.a);
+//    }
+//    @Test
+//    void Test8() {
+//        assertEquals(1, main.a);
+//    }
+//    @Test
+//    void Test9() {
+//        assertEquals(1, main.a);
+//    }
 
 }
